ae6726c4b7c6b9c0105e8c59c05d04884b355db2
Stephen Docy <sdocy@comcast.net>
2018-02-23 16:43:27 -0800

    added keyboard navigation shortcuts, fixed README

---
+++ Assets/scripts/keyboardShortcuts.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class keyboardShortcuts : MonoBehaviour {
+
+    public string myKeyPress;                   // set in editor per button
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(myKeyPress)) {
+            ColorBlock colors = GetComponent<Button>().colors;
+            colors.normalColor = Color.white;
+            GetComponent<Button>().colors = colors;
+            GetComponent<Button>().onClick.Invoke();
+        }
+    }
+}
